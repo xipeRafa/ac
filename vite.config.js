01078e6d345842995cde28520d866f622cf015e1
fer <fer@fers-iMac.local>
2025-06-16 12:00:54 -0700

    koko

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import MillionLint from "@million/lint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   base: "https://xipeRafa.github.io/ac"
+    plugins: [MillionLint.vite({auto:true}), react()],
+    base: "https://xipeRafa.github.io/ac"
 })
+
+
